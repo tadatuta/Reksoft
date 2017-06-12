567442d1f5ff83702e98af2fb4002a3064c89496
vnmityakova <vnmityakova@gmail.com>
2017-06-12 18:48:29 +0300

    prepare for new slides

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,38 @@
 require( [ "bootstrap", "device", "fabric" ], function() {
     // Проверим подгрузилась ли вместе с "bootstrap" его зависимость - jQuery
-    console.info( "jQuery exists: " + (typeof jQuery == "function" ? true : false ) );    
+    // console.info( "jQuery exists: " + (typeof jQuery == "function" ? true : false ) );    
 });
 
 
-require( [ "initial-block/initialBlockVIew", "bootstrap" ], function( MainView ) {
+require( [ "initial-block/initialBlockVIew", 
+          "we-like-block/weLikeView", 
+          "our-client-right/view", 
+          "spektr/view", 
+          "people/view", 
+          "find-yourself/view", 
+          "bootstrap" ], function( 
+         MainView, 
+         WeLikeView, 
+         ClientRightView, 
+         SpektrView,
+         PeopleView,
+         FindYourselfView) {
     var mainView = new MainView();
     $("#content").append( mainView.render().$el );
+    
+    var weLikeView = new WeLikeView();
+    $("body").append( weLikeView.render().$el ); 
+    
+    var clientRightView = new ClientRightView();
+    $("body").append( clientRightView.render().$el ); 
+    
+    var spektrView = new SpektrView();
+    $("body").append( spektrView.render().$el ); 
+    
+    var peopleView = new PeopleView();
+    $("body").append( peopleView.render().$el );
+    
+    var findYourselfView = new FindYourselfView();
+    $("body").append( findYourselfView.render().$el );
+    
 });
