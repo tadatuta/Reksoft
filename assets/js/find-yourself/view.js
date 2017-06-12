656356a61357c43670cfdc1c086e70badb6b6692
vnmityakova <vnmityakova@gmail.com>
2017-06-12 18:51:36 +0300

    prepare for new slides

---
+++ assets/js/find-yourself/view.js
@@ -0,0 +1,23 @@
+define([
+    "backbone",    
+    "text!../find-yourself/template.html"
+], function( Backbone, tpl ) {
+
+    var view = Backbone.View.extend({
+        // Кэшируем html-шаблон
+        template : _.template( tpl ),
+
+        initialize : function( options ) {
+
+        },
+
+        render : function() {
+            // Отключаем привязанные события, очищаем элемент и добавляем в верстку шаблон
+            this.$el.empty().append( this.template() );
+            return this;
+        }
+
+    });
+
+    return view;
+});
