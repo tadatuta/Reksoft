48d5f853c8ce43285111076603c0a8bdb1611b16
vnmityakova <vnmityakova@gmail.com>
2017-10-15 12:50:22 +0300

    added slides of Irina

--- assets/js/find-yourself/view.js
+++ assets/js/find-yourself/view.js
@@ -8,12 +8,21 @@ define([
         template : _.template( tpl ),
         className: "unit6",
         initialize : function( options ) {
-
         },
 
         render : function() {
             // Отключаем привязанные события, очищаем элемент и добавляем в верстку шаблон
             this.$el.empty().append( this.template() );
+            /*$('#name').focus(); // TODO подумать над нач. фокусом
+            $('#name').blur(function () {
+                $('#city').focus();
+            });
+            $('#city').blur(function () {
+                $('#question').focus();
+            });
+            $('#question').blur(function () {
+                $('#email').focus();
+            });*/
             return this;
         }
 
