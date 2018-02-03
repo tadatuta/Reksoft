0cc89171d6ee337b535f9c37d460423450d2dced
vnmityakova <vnmityakova@gmail.com>
2018-02-03 21:03:08 +0300

    add php libs

--- send.php
+++ send.php
@@ -12,5 +12,23 @@ $mail->isSMTP();
 // 2 = client and server messages
 $mail->SMTPDebug = 2;
 //Set the hostname of the mail server
-echo "OK";
+$mail->Host = 'mail.reksoft.ru';
+//Set the SMTP port number - likely to be 25, 465 or 587
+$mail->Port = 25;
+//Whether to use SMTP authentication
+$mail->SMTPAuth = false;
+//Set who the message is to be sent from
+$mail->setFrom('recognition@reksoft.ru', 'Recognition');
+//Set who the message is to be sent to
+$mail->addAddress('vnfoteyeva@gmail.com', '');
+//Set the subject line
+$mail->Subject = 'Recognition message';
+//Replace the plain text body with one created manually
+$mail->AltBody = 'This is a plain-text message body';
+//send the message, check for errors
+if (!$mail->send()) {
+    echo 'Mailer Error: ' . $mail->ErrorInfo;
+} else {
+    echo 'Message sent!';
+}
 ?>
