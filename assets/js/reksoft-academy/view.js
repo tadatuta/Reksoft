91ab3e739d33bff39edace165f20fe1513e8adc1
vnmityakova <vnmityakova@gmail.com>
2018-02-04 20:32:53 +0300

    animation added (добро пожаловать и академия)

--- assets/js/reksoft-academy/view.js
+++ assets/js/reksoft-academy/view.js
@@ -17,6 +17,9 @@ define([
         render : function() {
             // Отключаем привязанные события, очищаем элемент и добавляем в верстку шаблон
             this.$el.empty().append( this.template() );
+            setTimeout(function(){
+                $(".addFlashit5").addClass("flashitFull");
+            }, 5000);
             return this;
         },
 
