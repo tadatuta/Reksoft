975118a45f2ac116811ea9683f791046109e1435
Foteyeva Viktoria <mityakova@reksoft.ru>
2018-07-13 13:47:21 +0300

    email changed

--- send.php
+++ send.php
@@ -28,7 +28,7 @@ $mail->SMTPAuth = false;
 //Set who the message is to be sent from
 $mail->setFrom('recognition@reksoft.ru', 'Recognition');
 //Set who the message is to be sent to
-$mail->addAddress('vnfoteyeva@gmail.com', '');
+$mail->addAddress('khalupko@reksoft.ru', 'vnfoteyeva@gmail.com');
 //Set the subject line
 $mail->Subject = 'Recognition message';
 //Replace the plain text body with one created manually
